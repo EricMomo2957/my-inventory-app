23a46f1c133e585bdc0e141abe435c0dcab4026f
EricMomo2957 <Momoe2957@gmail.com>
2026-02-14 22:50:41 +0800

    Clerk setting and setting

--- frontend/src/pages/clerk/clerkSetting.jsx
+++ frontend/src/pages/clerk/clerkSetting.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import { useTheme } from '../context/ThemeContext'; 
+import { useTheme } from '../../context/ThemeContext'; 
 
 export default function ClerkSetting() {
   const { isDark, toggleTheme } = useTheme();
